c993a7e08151978895bd3ee19a2fd110fb090ab4
Mark Dewey <mark.t.dewey@gmail.com>
2017-08-16 13:32:26 -0400

    first commit

---
+++ Contact.cs
@@ -0,0 +1,24 @@
+namespace Files
+{
+    class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public override string ToString()
+        {
+            return $"{Name},{Email},{PhoneNumber}";
+        }
+        public Contact()
+        {
+
+        }
+        public Contact(string[] data)
+        {
+            Name = data[0];
+            Email = data[1];
+            PhoneNumber = data[2];
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
